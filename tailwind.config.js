e09f57ed2d3bbba3f5ba929e570939ea477e3bed
Luise Bräuer <madame.moon@gmx.de>
2023-12-07 11:23:50 +0100

    first steps

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./dist/**/*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      platinum: "#ECEAE9",
+      melon: "#FAB39D",
+      sky_blue: "#97C1D8",
+      paynes_gray: "#426676",
+      prussian_blue: "#093149",
+      alternative_blue: "#0B3B57",
+    },
   },
   plugins: [],
-}
+};
